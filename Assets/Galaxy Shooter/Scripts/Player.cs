3d687a214c53d4d78001053d1e592cca4ff5e4f0
Yvan Econa <ayecona@hotmail.com>
2018-04-22 17:50:56 -0400

    Galaxy Shooter Player Setup

---
+++ Assets/Galaxy Shooter/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+
+        transform.position = new Vector3(0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        movement();
+    }
+
+    private void movement(){
+
+        float horinzontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * speed * horinzontalInput * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
+
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.x < -8.2f)
+        {
+            transform.position = new Vector3(8.2f, transform.position.y, 0);
+        }
+        else if (transform.position.x > 8.2f)
+        {
+            transform.position = new Vector3(-8.2f, transform.position.y, 0);
+        }
+        else if (transform.position.y < -4.1f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.1f, 0);
+        }
+
+    }
+}
